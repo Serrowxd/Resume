8aa1e70ae9db0e9c8bade1de1e5b91cad5837d9d
Kevin Jolley <30558808+Serrowxd@users.noreply.github.com>
2018-04-09 22:46:43 -0500

    Updates

--- src/components/Typist/TypistText.js
+++ src/components/Typist/TypistText.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Typist from 'react-typist';
+import { Container, Row, Col } from 'reactstrap';
+
 import './TypistText.css';
 
 class TypistText extends React.Component {
@@ -14,16 +16,44 @@ class TypistText extends React.Component {
 
   render() {
     return (
-      <div className="TypistExample">
-        <Typist
-          className="TypistExample-header"
-          avgTypingSpeed={800}
+      <Typist>
+        <div
+          className="typistText-header"
+          avgTypingDelay={80}
           startDelay={2000}
           onTypingDone={this.onHeaderTyped}
         >
-          Serrow (OvO")
-        </Typist>
-      </div>
+          <p className="type1">Serrow d-(OvO")z</p>
+          <p className="type2"> Skills ヾ(｡^ω^｡)ノ</p>
+          <Container className="typeRow">
+            <Row>
+              <Col>
+                <h2 className="type3"> Languages </h2>
+                <ul className="type4">
+                  <li> JavaScript </li>
+                  <li> HTML5 </li>
+                  <li> CSS/LESS/SCSS </li>
+                </ul>
+              </Col>
+              <Col>
+                <h2 className="type3"> Front-End </h2>
+                <ul className="type4">
+                  <li> React.js </li>
+                  <li> Bootstrap </li>
+                </ul>
+              </Col>
+              <Col>
+                <h2 className="type3"> Back-End </h2>
+                <ul className="type4">
+                  <li> Node.js </li>
+                  <li> MongoDB </li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+          <p className="checkMe"> Check me out! </p>
+        </div>
+      </Typist>
     );
   }
 }
